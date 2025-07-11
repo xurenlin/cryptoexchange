e8e5fa31b9e25bf91f0009defca5f2b4fa49911f
xurenlin <xurenlin>
2025-07-11 17:11:15 +0800

    集成nacos 配置中心

--- account-service/src/main/java/com/xrl/cryptoexchange/account/user/controller/UserController.java
+++ account-service/src/main/java/com/xrl/cryptoexchange/account/user/controller/UserController.java
@@ -3,7 +3,9 @@ package com.xrl.cryptoexchange.account.user.controller;
 import com.xrl.cryptoexchange.account.user.feignClient.TradeFeignClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +14,41 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/user")
 @Slf4j
+@RefreshScope
 public class UserController {
 
-    @Resource
-    private DiscoveryClient discoveryClient;
 
     @Autowired
     TradeFeignClient tradeFeignClient;
+
+    @Value("${account}")
+    private String account;
+
+    @Value("${id1}")
+    private String id;
+
+    @Value("${log1}")
+    private String logs;
 
     @RequestMapping("/query")
     public String query(){
         return tradeFeignClient.query();
     }
+
+    @RequestMapping("/account")
+    public String account(){
+        return account;
+    }
+
+    @RequestMapping("/id")
+    public String id(){
+        return id;
+    }
+
+    @RequestMapping("/log")
+    public String log(){
+        return logs;
+    }
+
+
 }
